fae3f8f092e31924e8f1000cd79532ceadc4a4f6
Reidar Nilsen <4reidar@gmail.com>
2024-01-24 22:17:07 +0100

    Add project files.

---
+++ ArtistApi/Data/ArtistDbContext.cs
@@ -0,0 +1,15 @@
+﻿using ArtistApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ArtistApi.Data
+{
+    public class ArtistDbContext : DbContext
+    {
+        public ArtistDbContext(DbContextOptions<ArtistDbContext> options) : base(options)
+        {
+            
+        }
+        public DbSet<Artist> Artists { get; set; }
+        
+    }
+}
---
+++ ArtistApi/Models/Artist.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ArtistApi.Models
+{
+    public class Artist
+    {
+        [Key]
+        public int ArtistId { get; set; }
+        [Required]
+        [MaxLength(40, ErrorMessage = "Artist name must be 40 characters or less"), MinLength(2)]
+        public string ArtistName { get; set; }
+    }
+}
